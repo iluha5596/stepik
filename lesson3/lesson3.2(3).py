f0f59ed4b3f94132a5b6c890c2a8a731ce6dbd7c
iluha5596 <pantik96@mail.ru>
2023-12-05 16:58:09 +0300

    add lesson3

---
+++ lesson3/lesson3.2(3).py
@@ -0,0 +1,34 @@
+import time
+import unittest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+
+
+class TestAbs(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def testAbs1(self):
+        URL = 'http://suninjuly.github.io/registration1.html'
+        driver.get(URL)
+        inputLastNameElement = driver.find_element(By.XPATH, '(//div/label)[2]')
+        inputLastName = inputLastNameElement.text
+        self.assertEqual(inputLastName, 'Last name*', f'Ожидалось наличие поля "Last name*"')
+
+
+    def testAbs2(self):
+        SecondURL = 'http://suninjuly.github.io/registration2.html'
+        driver.get(SecondURL)
+        inputLastNameElement = driver.find_element(By.XPATH, '(//div/label)[2]')
+        inputLastName = inputLastNameElement.text
+        self.assertEqual(inputLastName, 'Last name*', f'Ожидалось наличие поля "Last name*"')
+
+if __name__ == '__main__':
+    unittest.main()
